be1d610d22220d66ff71294ac8818f93d7b8655d
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-05-11 18:09:10 -0400

    Learning how to add navbar + related components

--- the-cross-send-app/src/App.js
+++ the-cross-send-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {Container, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Dropdown } from 'bootstrap';
+import {Container, Button, cols, rows, form} from 'react-bootstrap';
 
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <div>
-          <container className="App-header">
-            <h1>Commercial Real estate in one place</h1>
-<Dropdown className="Dropdown">User Name</Dropdown>
-          </container>
-        </div>
-        <button>Test button</button>
-      </header>
+          <h1>Commercial Real estate in one place</h1>
     </div>
+
   );
 }
 
