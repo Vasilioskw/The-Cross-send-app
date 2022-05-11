20999be2b5754041387af63d8dab632c8e3d3d1b
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-05-11 16:23:19 -0400

    bootstrap css successfull, changes noticed

--- the-cross-send-app/src/App.js
+++ the-cross-send-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button>Test button</button>
       </header>
     </div>
   );
