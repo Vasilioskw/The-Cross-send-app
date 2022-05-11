96ea1dfd7e35a6665868439fe4c574277c37dc31
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-05-11 16:57:17 -0400

    check why page isn't endering

--- the-cross-send-app/src/App.js
+++ the-cross-send-app/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import {Container, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'bootstrap';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <container className="App-header">
+            <h1>Commercial Real estate in one place</h1>
+<Dropdown className="Dropdown">User Name</Dropdown>
+          </container>
+        </div>
         <button>Test button</button>
       </header>
     </div>
