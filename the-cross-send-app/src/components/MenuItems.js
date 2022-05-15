11b1e49e837c9d0673e5d9705528ce154f365fa7
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-05-15 18:34:13 -0400

    more work on the navbar

---
+++ the-cross-send-app/src/components/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Email',
+    path: '/email',
+    cName: 'dropdown-link'
+	},
+	{
+    title: 'Email',
+    path: '/email',
+    cName: 'dropdown-link'
+	}, 
+	{
+    title: 'Email',
+    path: '/email',
+    cName: 'dropdown-link'
+	}, 
+	{
+    title: 'Email',
+    path: '/email',
+    cName: 'dropdown-link'
+	}, 
+	{
+    title: 'Email',
+    path: '/email',
+    cName: 'dropdown-link'
+	}
+];
--- the-cross-send-app/src/components/Navbar.js
+++ the-cross-send-app/src/components/Navbar.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
-import'./Navbar.css'
-import Dropdown from './Dropdown'
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import'./Navbar.css';
+import Dropdown from './Dropdown';
 
 
 function Navbar() {
 	const [click, setClick]= useState(false);
+	const [dropdown, setDropdown] = useState(false); 
 
 	const handleClick = () => setClick(!click);
 	const closeMobileMenu = () => setClick(false);
@@ -17,7 +18,6 @@ return(
 			<Link to='/' className='navbar-logo'>
 				Commercial Real estate in one place
 			</Link> 
-
 			<div className='menu-icon' onClick={handleClick}>
 				<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
 			</div>	
@@ -28,12 +28,33 @@ return(
 					</Link>
 				</li>
 				<li className='nav-item'>
-					<Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-						services <i className='fas fa-caret-down' />
+					<Link to='/email' className='nav-links' onClick={closeMobileMenu}>
+						Email <i className='fas fa-caret-down' />
+					</Link>
+					{dropdown && <Dropdown />}
+				</li>
+				<li className='nav-item'>
+					<Link to='/profile' className='nav-links' onClick={closeMobileMenu}>
+						Profile
 					</Link>
 				</li>
-
+				<li className='nav-item'>
+					<Link to='/account' className='nav-links' onClick={closeMobileMenu}>
+						Account
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+						login / Sign up
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link to='/faq' className='nav-links' onClick={closeMobileMenu}>
+						FAQ
+					</Link>
+				</li>
 			</ul>
+			<Button />
 		</nav>
   	</>
   );
